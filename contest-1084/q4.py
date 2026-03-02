4d19e9677214c417708265032aeb3af4a284302d
James Peralta <jamesperalta.swe@gmail.com>
2026-03-02 08:56:39 -0800

    UPSOLVE Q4!!

--- contest-1084/q4.py
+++ contest-1084/q4.py
@@ -3,16 +3,46 @@ input = sys.stdin.readline
 tests = int(input())
 
 for _ in range(tests):
-    _ = int(input())
+    first = input().split()
+    x = int(first[1])
+    y = int(first[2]) 
     nums = [int(elem) for elem in input().split()]
 
-    is_inc = True
-    for i in range(1, len(nums)):
-        if nums[i] < nums[i - 1]:
-            is_inc = False
+    inside = []
+    outside = []
+    for i in range(len(nums)):
+        if i < x:
+            outside.append(nums[i])
+        elif i >= y:
+            outside.append(nums[i])
+        else:
+            inside.append(nums[i])
+    
+    # print(inside)
+    # print(outside)
+    min_inside = inside.index(min(inside))
+    min_val = min(inside)
+    new_inside = inside[min_inside:] + inside[:min_inside]
+    
+    res = []
+    index_o = 0
+    while index_o < len(outside):
+        if outside[index_o] <= min_val:
+            res.append(outside[index_o])
+        else:
             break
 
-    if is_inc:
-        print(len(nums))
-    else:
-        print(1)
+        index_o += 1
+    
+    res += new_inside
+    while index_o < len(outside):
+        res.append(outside[index_o])
+        index_o += 1
+    
+    print(" ".join([str(elem) for elem in res]))
+
+"""
+1
+3 1 2
+3 2 1
+"""
